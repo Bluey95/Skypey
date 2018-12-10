5e3e78b2ec14055d3e08361fc936eb7d2cc7ef98
Bluey95 <blue.were@yahoo.com>
2018-12-10 11:28:41 +0300

    (feat(contacts): add contacts sidebar [Delivers #162537887]

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -2,12 +2,16 @@ import Sidebar from '../Sidebar';
 import Main from '../Main';
 import './index.css'
 import React from 'react';
+import store from '../../store';
+import _ from 'lodash';
+
+const { contacts } = store.getState();
 
 
 const App = () => {
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar contacts={_.values(contacts)} />
       <Main />
     </div>
   );
